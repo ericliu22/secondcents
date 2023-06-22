d2b6ff3272977c07c4ba329facb893df78440c52
ericliu22 <102707906+ericliu22@users.noreply.github.com>
2023-06-22 17:09:03 +0800

    Small change to setup script

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Hello this is the new GitHub respoitory for the app
 
 ## Setup instructions
 Go to terminal and run the following to get all the mongodb packages
+(make sure you're in the right directory)
 '''
 chmod +x setup.sh
 ./setup.sh
--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ if ! which flutter > /dev/null; then
       cd ~
       git clone https://github.com/flutter/flutter.git -b stable
       echo '\nPATH="~/flutter/bin:${PATH}"\nexport PATH\n' >> .zprofile
+      sudo gem update --system
+      sudo gem install cocoapods
     else
       echo "Please install flutter (or add it to your path)"
       exit 1
@@ -30,6 +32,7 @@ brew tap mongodb/brew
 brew install mongodb-community@6.0
 cd ~/Downloads
 curl -O https://downloads.mongodb.com/compass/mongodb-compass-1.38.0-darwin-x64.dmg
-open mongodb-compass-1.38.0-darwin-x64.dmg 
+open mongodb-compass-1.38.0-darwin-x64.dmg
+say Done installing you bitch
 sleep 15
-rm mongodb-compass-1.38.0-darwin-x64.dmg  
+rm mongodb-compass-1.38.0-darwin-x64.dmg
