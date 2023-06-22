e365bdbc153a34a51ed8d55488c8de57417fe8c8
ericliu22 <102707906+ericliu22@users.noreply.github.com>
2023-06-22 17:05:48 +0800

    Some setup stuff
    -Can run a setup script to install everything
    -Check README for instructions to install

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # secondcents
 
 Hello this is the new GitHub respoitory for the app
+
+## Setup instructions
+Go to terminal and run the following to get all the mongodb packages
+'''
+chmod +x setup.sh
+./setup.sh
+'''
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+xcode-select --install
+
+if ! which flutter > /dev/null; then
+   echo -e "Flutter not installed! Install? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+      cd ~
+      git clone https://github.com/flutter/flutter.git -b stable
+      echo '\nPATH="~/flutter/bin:${PATH}"\nexport PATH\n' >> .zprofile
+    else
+      echo "Please install flutter (or add it to your path)"
+      exit 1
+   fi
+fi
+
+if ! which brew > /dev/null; then
+   echo -e "Homebrew not installed! Install? (y/n) \c"
+   read
+   if "$REPLY" = "y"; then
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    else
+      echo "Please install homebrew to continue"
+      exit 1
+   fi
+fi
+
+brew tap mongodb/brew
+brew install mongodb-community@6.0
+cd ~/Downloads
+curl -O https://downloads.mongodb.com/compass/mongodb-compass-1.38.0-darwin-x64.dmg
+open mongodb-compass-1.38.0-darwin-x64.dmg 
+sleep 15
+rm mongodb-compass-1.38.0-darwin-x64.dmg  
