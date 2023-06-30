4ae452565f0b595df1f0cc06d5b38c937a9bfdfd
ericliu22 <102707906+ericliu22@users.noreply.github.com>
2023-06-30 11:14:25 +0800

    Added MONGO_URL for connection

--- flutter/secondcents/lib/mongodb.dart
+++ flutter/secondcents/lib/mongodb.dart
@@ -2,7 +2,8 @@ import 'dart:developer';
 
 import 'package:mongo_dart/mongo_dart.dart';
 
-const MONGO_URL = ""; //Insert URL here later
+const MONGO_URL =
+    "mongodb+srv://secondcents:<Two_C3nt\$_App_2023>@secondcents-1.aovv9i7.mongodb.net/"; //Insert URL here later
 
 class MongoDatabase {
   static connect() async {
