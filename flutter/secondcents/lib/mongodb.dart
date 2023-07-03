3af482989146c95ca027b4e3931c80234055b402
ericliu22 <102707906+ericliu22@users.noreply.github.com>
2023-07-03 10:36:46 +0800

    Added realm
    -Realm will be how we access the database

--- flutter/secondcents/lib/mongodb.dart
+++ flutter/secondcents/lib/mongodb.dart
@@ -1,7 +1,10 @@
 import 'dart:developer';
 
 import 'package:mongo_dart/mongo_dart.dart';
+import "package:realm/realm.dart" as realm;
+import "package:realm/realm.dart";
 
+// ignore: constant_identifier_names
 const MONGO_URL =
     "mongodb+srv://secondcents:<Two_C3nt\$_App_2023>@secondcents-1.aovv9i7.mongodb.net/"; //Insert URL here later
 
@@ -13,4 +16,14 @@ class MongoDatabase {
     var status = db.serverStatus();
     print(status);
   }
+}
+
+@RealmModel()
+class User {
+  @PrimaryKey()
+  late realm.ObjectId userId;
+
+  late String username;
+  late String displayName;
+  late String profilePic;
 }
