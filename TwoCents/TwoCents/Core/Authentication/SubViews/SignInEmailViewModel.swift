1734401f3839bbfa49e4c60e5dca02683ef8cf6e
mlgxd420 <jdidi473@yahoo.com>
2023-08-10 21:19:13 +0800

    fixed typo bug

--- TwoCents/TwoCents/Core/Authentication/SubViews/SignInEmailViewModel.swift
+++ TwoCents/TwoCents/Core/Authentication/SubViews/SignInEmailViewModel.swift
@@ -23,7 +23,7 @@ final class SignInEmailViewModel: ObservableObject{
         let authDataResult = try await AuthenticationManager.shared.createUser(email: email, password: password)
 //        try await UserManager.shared.createNewUser(auth: authDataResult)
         
-        let user = DBUser(auth: AuthDataResultModel)
+        let user = DBUser(auth: authDataResult)
         try await UserManager.shared.createNewUser(user: user)
       
         
