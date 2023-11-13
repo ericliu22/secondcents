3dcfc17ca30c6dad814a1945d2e4172b13dccbbb
mlgxd420 <jdidi473@yahoo.com>
2023-11-13 17:42:39 -0500

    Added profile photo for enzo vote view

--- TwoCents/TwoCents/Enzo/VoteGame/VoteGameView.swift
+++ TwoCents/TwoCents/Enzo/VoteGame/VoteGameView.swift
@@ -103,6 +103,50 @@ struct VoteGameView: View {
                                 
 //                                
                                 
+                                Group{
+                                    //Circle or Profile Pic
+                                    
+                                    let frameSize: CGFloat? = 64
+                                    
+                                    if let urlString = myMemberInfo[member].profileImageUrl,
+                                       let url = URL(string: urlString) {
+                                        
+                                        //If there is URL for profile pic, show
+                                        //circle with stroke
+                                        AsyncImage(url: url) {image in
+                                            image
+                                                .resizable()
+                                                .scaledToFill()
+                                            
+                                        } placeholder: {
+                                            //else show loading after user uploads but sending/downloading from database
+                                            ProgressView()
+                                                .progressViewStyle(CircularProgressViewStyle(tint: Color(UIColor.systemBackground)))
+                                                .frame(width: frameSize, height: frameSize)
+                                                .background(
+                                                    Circle()
+                                                        .fill(Color.accentColor)
+                                                )
+                                        }
+                                        .clipShape(Circle())
+                                        .frame(width: frameSize, height: frameSize)
+                                        
+                                    } else {
+                                        //if space does not have profile pic, show circle
+                                        Circle()
+                                            .fill(Color.accentColor)
+                                            .clipShape(Circle())
+                                            .frame(width: frameSize, height: frameSize)
+                                    }
+                                    
+                                    
+                                }
+                                
+                                
+                                
+                                
+                                
+                                
                             }
                         }
                         
