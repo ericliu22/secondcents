be23d76ab05b151f2631f1f6e34c4ba00f7c519f
ericliu22 <ericliu108@gmail.com>
2024-06-11 15:13:57 -0400

    Mini-change to index page

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,19 +27,19 @@ app.get('/api', ({ redirect }) => {
 })
 
 app.get('/', () => {
-	return "Home page";
+	return "Guoliang Eric Liu's webiste";
 })
 
 app.listen(process.env.PORT ?? 8080, () => {
 	console.log(`Server is running at on port ${app.server?.port}...`)
 });
 
+/*
 const testNotification = {
 	title: "I like james charles",
 	body: "Gyatt"
 };
 
-/*
 const testBody = {
 	to: "eZx-VtX3Lkm-iyK4Ky99rL:APA91bEvnTqEI0asvDcqYKxQPIurbjvUFIdxKDCjgssBRjWq8W8LU8VOilnoJ_TWClLKrToJY4if-UMsnUEQwSzuoHmjYq1wb4Bodtl5Qk05y4uee8HzXhbI9ySbwl-UmJcV5mnJPORs",
 	notification: testNotification
