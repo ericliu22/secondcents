5bce51d3afa185b36e37e98bf629d2b8917651b5
ericliu22 <ericliu108@gmail.com>
2024-08-09 14:18:30 -0400

    Uncommented Josh's calendar widget

--- TwoCents/TwoCents/Core/FrontPageView.swift
+++ TwoCents/TwoCents/Core/FrontPageView.swift
@@ -41,7 +41,6 @@ struct FrontPageView: View {
                 }
                 .tag(1)
 
-            /*
             CalendarWidget(widget: CanvasWidget(id: UUID(uuidString: "E2C85940-3266-44F7-B6D2-4D21F507B25C")!, width: .infinity, height:  .infinity, borderColor: .red, userId: "jisookim", media: .text, widgetName: "Text", widgetDescription: "A bar is a bar", textString: "Fruits can't even see so how my Apple Watch"), spaceId: "2FF491A4-CEC6-419F-A199-204810864FCF"
                             )
               
@@ -50,7 +49,6 @@ struct FrontPageView: View {
                     Text("Calendar")
                 }
                 .tag(2)
-             */
             
             
 //            CustomCalendarView(spaceId: "2FF491A4-CEC6-419F-A199-204810864FCF"
