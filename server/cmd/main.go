2c68c55f75ad9499ddc14092b60ca029e67ceb61
eliu22 <gel8846@nyu.edu>
2024-09-26 23:54:35 -0400

    Added credential path env

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,33 +1,37 @@
 package main
 
 import (
-    "context"
-    "log"
-    "os"
+	"context"
+	"log"
+	"os"
 
-    "firebase.google.com/go"
-    "github.com/valyala/fasthttp"
-    "server/internal"
-    "google.golang.org/api/option"
+	"firebase.google.com/go"
+	"github.com/valyala/fasthttp"
+	"server/internal"
+	"google.golang.org/api/option"
 )
 
 func main() {
-    // Initialize Firebase Admin SDK
-    opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
-    app, err := firebase.NewApp(context.Background(), nil, opt)
-    if err != nil {
-        log.Fatalf("error initializing Firebase app: %v", err)
-    }
+	// Initialize Firebase Admin SDK
+	credential_path := os.Getenv("FIREBASE_ADMIN_CREDENTIAL_PATH")
+	if credential_path == "" {
+		log.Fatalf("Firebase credential path not set")
+	}
+	opt := option.WithCredentialsFile(credential_path)
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing Firebase app: %v", err)
+	}
 
-    // Set up the router
-    router := internal.SetupRouter(app)
+	// Set up the router
+	router := internal.SetupRouter(app)
 
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8000" // Default port if not specified
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" // Default port if not specified
+	}
 
-    // Start the server
-    log.Fatal(fasthttp.ListenAndServe(":"+port, router.Handler))
+	    // Start the server
+	log.Fatal(fasthttp.ListenAndServe(":"+port, router.Handler))
 }
 
