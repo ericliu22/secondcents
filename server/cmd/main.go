2d9cc17a19804a620ec168e81becaaff1d7668fe
ericliu22 <ericliu108@gmail.com>
2024-09-30 23:34:20 -0400

    Log files are finally correct motherfucking timezone

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -21,6 +21,12 @@ func setupLogging() (*os.File, error) {
     if err != nil {
         return nil, err
     }
+
+    log.SetFlags(0) // Disable default timestamp (we'll add our own)
+    
+    // Define the logger prefix with the time in the desired timezone
+    log.SetPrefix(time.Now().In(loc).Format("2006-01-02 15:04:05 "))
+
     log.SetOutput(logFile)
     return logFile, nil
 }
