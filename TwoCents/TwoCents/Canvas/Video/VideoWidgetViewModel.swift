ed2a625525a7119f7017d89bfc1bf093a86087fb
mlgxd420 <jdidi473@yahoo.com>
2024-10-14 12:50:23 -0400

    new algo for thumbnail

--- TwoCents/TwoCents/Canvas/Video/VideoWidgetViewModel.swift
+++ TwoCents/TwoCents/Canvas/Video/VideoWidgetViewModel.swift
@@ -39,40 +39,40 @@ final class VideoWidgetViewModel {
 //        return
 //
 //    }
-//    
-//    func getVideoThumbnail(from url: URL, maxSize: CGSize = CGSize(width: TILE_SIZE * 2, height: TILE_SIZE * 2)) async {
-//        isLoading = true
-//        defer {
-//            isLoading = false
-//        }
-//
-//        // Create an AVAsset with only video tracks to reduce load time
-//        let asset = AVAsset(url: url)
-//        let assetImgGenerate = AVAssetImageGenerator(asset: asset)
-//        assetImgGenerate.appliesPreferredTrackTransform = true
-//        
-//        // Reduce the size of the thumbnail for faster generation
-//        assetImgGenerate.maximumSize = maxSize
-//        
-//        // Try to extract the image from an earlier time
-//        let time = CMTime(seconds: 0.1, preferredTimescale: 15) // Faster and lower precision
-//        
-//        // Generate the thumbnail with a higher concurrency priority
-//        do {
-//            let img = try await Task(priority: .high) { () -> CGImage in
-//                return try assetImgGenerate.copyCGImage(at: time, actualTime: nil)
-//            }.value
-//            
-//            // Update the UI thumbnail on the main thread
-//            await MainActor.run {
-//                videoThumbnail = UIImage(cgImage: img)
-//            }
-//            
-//        } catch {
-//            print("Error generating thumbnail: \(error.localizedDescription)")
-//            videoThumbnail = nil
-//        }
-//    }
+    
+    func getVideoThumbnail(from url: URL, maxSize: CGSize = CGSize(width: TILE_SIZE * 2, height: TILE_SIZE * 2)) async {
+        isLoading = true
+        defer {
+            isLoading = false
+        }
+
+        // Create an AVAsset with only video tracks to reduce load time
+        let asset = AVAsset(url: url)
+        let assetImgGenerate = AVAssetImageGenerator(asset: asset)
+        assetImgGenerate.appliesPreferredTrackTransform = true
+        
+        // Reduce the size of the thumbnail for faster generation
+        assetImgGenerate.maximumSize = maxSize
+        
+        // Try to extract the image from an earlier time
+        let time = CMTime(seconds: 0.1, preferredTimescale: 15) // Faster and lower precision
+        
+        // Generate the thumbnail with a higher concurrency priority
+        do {
+            let img = try await Task(priority: .high) { () -> CGImage in
+                return try assetImgGenerate.copyCGImage(at: time, actualTime: nil)
+            }.value
+            
+            // Update the UI thumbnail on the main thread
+            await MainActor.run {
+                videoThumbnail = UIImage(cgImage: img)
+            }
+            
+        } catch {
+            print("Error generating thumbnail: \(error.localizedDescription)")
+            videoThumbnail = nil
+        }
+    }
 
 
     
