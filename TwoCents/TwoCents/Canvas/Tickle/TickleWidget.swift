fb0671387d3cad5d076f599db5ba6a6412aa16f5
ericliu22 <ericliu108@gmail.com>
2025-02-19 15:41:19 -0500

    Fixed tickle widget

--- TwoCents/TwoCents/Canvas/Tickle/TickleWidget.swift
+++ TwoCents/TwoCents/Canvas/Tickle/TickleWidget.swift
@@ -15,32 +15,11 @@ struct TickleWidget: WidgetView {
 
     private let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
 
-    private func startHapticFeedback() {
-        feedbackGenerator.prepare()
-        var tickleCount = 0
-        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
-            if isPressing {
-                feedbackGenerator.impactOccurred()
-
-                tickleCount += 1
-                tickleString = String(tickleCount)
-            } else {
-                timer.invalidate()
-                tickleString = "Tickle"
-            }
-        }
-    }
-
-    private func stopHapticFeedback() {
-        // Ensure the feedback generator can be used again if needed
-        feedbackGenerator.prepare()
-    }
-
     init(widget: CanvasWidget) {
         self.widget = widget
     }
 
-    func ProfilePic(url: URL, targetUserColor: Color) -> some View {
+    func ProfilePic(url: URL) -> some View {
         //If there is URL for profile pic, show
         //circle with stroke
         CachedImage(imageUrl: url)
@@ -54,61 +33,42 @@ struct TickleWidget: WidgetView {
             if let member {
                 if let profileImageUrl = member.profileImageUrl {
                     ProfilePic(
-                        url: URL(string: profileImageUrl)!,
-                        targetUserColor: Color.fromString(
-                            name: member.userColor ?? "gray"))
+                        url: URL(string: profileImageUrl)!)
+                } else {
+                    Circle()
+                        .fill(
+                         Color.fromString(
+                            name: member.userColor ?? "gray")
+                        )
+                        .frame(width: 100, height: 100)
                 }
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(.thinMaterial)
-                        .aspectRatio(1, contentMode: .fit)
-                        .gesture(
-                            TapGesture()
-                                .onEnded { _ in
-                                    if let startTime =
-                                        pressStartTime
-                                    {
-                                        let duration = Date()
-                                            .timeIntervalSince(startTime)
-                                        let tickleCount = Int(duration * 10)
-
-                                        if !isPressing
-                                            && tickleCount <= 1
-                                        {
-
-                                            feedbackGenerator.impactOccurred()
-
-                                            let currentUserId =
-                                                try! AuthenticationManager
-                                                .shared
-                                                .getAuthenticatedUser().uid
-                                            //
-                                            //
-                                            Task {
-                                                try await tickleNotification(
-                                                    userId: widget.userId,
-                                                    count: tickleCount)
-                                            }
-                                            AnalyticsManager.shared.tickle(
-                                                count: tickleCount)
-                                        }
-
-                                    }
-
-                                }
-                        )
-                    Text(tickleString)
-                        .font(
-                            tickleString == "Tickle"
+                Button {
+                    feedbackGenerator.impactOccurred()
+                    Task {
+                        try await tickleNotification(
+                            userId: widget.userId,
+                            count: 1)
+                    }
+                    AnalyticsManager.shared.tickle(
+                        count: 1)
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.thinMaterial)
+                            .aspectRatio(1, contentMode: .fit)
+                        Text(tickleString)
+                            .font(
+                                tickleString == "Tickle"
                                 ? .title2 : .largeTitle
-                        )
-                        .fontWeight(.bold)
-                        .fontDesign(
-                            tickleString == "Tickle"
+                            )
+                            .fontWeight(.bold)
+                            .fontDesign(
+                                tickleString == "Tickle"
                                 ? .default : .monospaced
-                        )
-                        .foregroundStyle(.secondary)
-                        .frame(width: .infinity, height: 50)
+                            )
+                            .foregroundStyle(.secondary)
+                            .frame(width: .infinity, height: 50)
+                    }
                 }
             }
         }
